745c44e24051d4b5638daa53c255923d12362b7c
vinit4a <vinit.k.addweb@gmail.com>
2021-09-09 12:35:10 +0530

    08.09.2021 task

--- 08.09.2021/4.contructor.php
+++ 08.09.2021/4.contructor.php
@@ -14,3 +14,22 @@ class l extends p
 }
 $o = new l();
 ?>
+<?php
+
+class p
+{
+    public function __construct()
+    {
+        
+        echo "hello <br>";
+    }
+}
+class l extends p
+{
+  function v(){
+      echo"hi ";
+  }
+}
+$o = new l();
+$o->v();
+?>
